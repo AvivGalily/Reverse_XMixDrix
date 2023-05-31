f9dd8d8232a5360ce43cd088088decdb56259897
AvivGalily <105667180+AvivGalily@users.noreply.github.com>
2023-05-31 20:55:01 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using Ex02_X_Mix_Drix;
+
+namespace Ex02_X_Mix_Drix
+{
+    class Program
+    {
+        public static void Main()
+        {
+            UserInterface xMixDrixGame = new UserInterface();
+            xMixDrixGame.Run();
+        }
+    }
+}
